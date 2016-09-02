fe7c45c86c202dd8b285aa9d0b8b97b8e444fd93
Fernando Bevilacqua <dovyski@gmail.com>
2016-09-02 13:05:56 +0200

    Basic main program

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string.h>
+
+#define VERSION	"1.0.0"
+
+void copyright() {
+	std::cout << "BDFextractor - Version " << VERSION << " (build: " << __DATE__ << ")" << std::endl;
+	std::cout << "Copyright (c) 2016, Fernando Bevilacqua <fernando.bevilacqua@his.se>" << std::endl << std::endl;
+}
+
+void usage() {
+	std::cout << "Usage: BDFextractor.exe [options]" << std::endl << std::endl;
+	std::cout << "options:" << std::endl << std::endl;
+
+	std::cout << " --option            Info about option." << std::endl;
+	std::cout << " --option2           Another info this option as well." << std::endl;
+}
+
+int main(int argc, const char *argv[])
+{
+	copyright();
+
+	if (argc > 1 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)) {
+		usage();
+		exit(2);
+	}
+
+	std::cout << "Hello world!" << std::endl;
+	getchar();
+
+	exit(0);
+}
